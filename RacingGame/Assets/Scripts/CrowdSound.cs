1784a9dd56ead89a682fbbe93b45caee71b86463
alexandrudecean <147138112+alexandrudecean@users.noreply.github.com>
2025-01-07 21:20:30 +0200

    added crowd sound

---
+++ RacingGame/Assets/Scripts/CrowdSound.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CrowdSound : MonoBehaviour
+{
+    public AudioSource crowdAudio; 
+    public Transform carTransform; 
+    public float maxDistance = 100.0f; 
+
+    private void Start()
+    {
+        if (crowdAudio == null)
+        {
+            crowdAudio = GetComponent<AudioSource>();
+        }
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(carTransform.position, transform.position);
+
+        Debug.Log("Distance to car: " + distance); 
+
+        if (distance <= maxDistance)
+        {
+            if (!crowdAudio.isPlaying)
+            {
+                crowdAudio.Play();
+            }
+
+            crowdAudio.volume = Mathf.Lerp(1.0f, 0.0f, distance / maxDistance);
+        }
+        else
+        {
+            if (crowdAudio.isPlaying)
+            {
+                crowdAudio.Stop();
+            }
+        }
+    }
+
+}
